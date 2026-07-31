546e4e40c427983f541a95d36c94e5c2ca66ad99
Ankit Raj <ankit13082005raj@gmail.com>
2026-08-01 00:23:18 +0530

    vector basics

---
+++ la1.py
@@ -0,0 +1,12 @@
+import numpy as np
+v = np.array([1,-1,2])
+w = np.array([2,5,2])
+print(v+w)
+print(v-w)
+#scalar multi:
+print(3*v)
+#len of vector:
+print(np.linalg.norm(v))
+#dot product
+s=np.dot(v,w)
+print(setattr)
